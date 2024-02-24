d085777ae5191955984b479d7aeb6d8c6868c73b
R-Hambly-TS <55469422+R-Hambly-TS@users.noreply.github.com>
2024-02-24 16:31:45 -0400

    Create player.dart

---
+++ player.dart
@@ -0,0 +1,94 @@
+import 'package:flame/game.dart';
+import 'package:flame/components.dart';
+import 'package:flutter/services.dart';
+import 'package:steel_strategy/steel_strategy.dart';
+
+enum PlayerState { idle, running }
+
+class Player extends SpriteAnimationGroupComponent
+    with HasGameRef<SteelStrategy>, KeyboardHandler {
+  String character;
+  Player({position, this.character = 'Ninja Frog'}) : super(position: position);
+
+  late final SpriteAnimation idleAnimation;
+  late final SpriteAnimation runningAnimation;
+  final double stepTime = 0.05;
+
+  double horizontalMovement = 0;
+  double moveSpeed = 100;
+  Vector2 velocity = Vector2.zero();
+  List<CollisionBlock> collisionBlocks = [];
+
+  @override
+  FutureOr<void> onLoad() {
+    _loadAllAnimations();
+    debugMode = true;
+    return super.onLoad();
+  }
+
+  @override
+  void update(double dt) {
+    _updatePlayerState();
+    _updatePlayerMovement(dt);
+
+    super.update(dt);
+  }
+
+  @override
+  bool onKeyEvent(RawKeyEvent event, Set<LogicalKeyboardKey> keysPressed) {
+    horizontalMovement = 0;
+    final isLeftKeyPressed = keysPressed.contains(LogicalKeyboardKey.keyA) ||
+        keysPressed.contains(LogicalKeyboardKey.arrowLeft);
+    final isRightKeyPressed = keysPressed.contains(LogicalKeyboardKey.keyD) ||
+        keysPressed.contains(LogicalKeyboardKey.arrowRight);
+
+    horizontalMovement += isLeftKeyPressed ? -1 : 0;
+    horizontalMovement += isRightKeyPressed ? 1 : 0;
+
+    return super.onKeyEvent(event, keysPressed);
+  }
+
+  void _loadAllAnimations() {
+    idleAnimation = _spriteAnimation('Idle', 11);
+    runningAnimation = _spriteAnimation('Run', 12);
+    // List of all animations
+    animations = {
+      PlayerState.idle: idleAnimation,
+      PlayerState.running: runningAnimation
+    };
+
+    // Set current animation
+    current = PlayerState.idle;
+  }
+
+  SpriteAnimation _spriteAnimation(String state, int amount) {
+    return SpriteAnimation.fromFrameData(
+      game.images.fromCache('Main Characters/$character/$state (32x32).png'),
+      SpriteAnimationData.sequenced(
+        amount: amount,
+        stepTime: stepTime,
+        textureSize: Vector2.all(32),
+      ),
+    );
+  }
+
+  void _updatePlayerMovement(double dt) {
+    velocity.x = horizontalMovement * moveSpeed;
+    position.x += velocity.x * dt;
+  }
+
+  void _updatePlayerState() {
+    PlayerState playerState = PlayerState.idle;
+
+    if (velocity.x < 0 && scale.x > 0) {
+      flipHorizontallyAroundCenter();
+    } else if (velocity.x > 0 && scale.x < 0) {
+      flipHorizontallyAroundCenter();
+    }
+
+    // Check if moving and set to running
+    if (velocity.x > 0 || velocity.x < 0) playerState = PlayerState.running;
+
+    current = playerState;
+  }
+}
