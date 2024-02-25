63e5049c18d9b7021514597564a1f30abaf0aea4
R-Hambly-TS <55469422+R-Hambly-TS@users.noreply.github.com>
2024-02-25 15:52:57 -0400

    Update splash_screen.dart

--- lib/views/splash_screen/splash_screen.dart
+++ lib/views/splash_screen/splash_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:steel-strategy/core/widgets/bg_widget.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -10,10 +11,23 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center (
-        child: Image.asset(Assets.images.banner),
-      ), // Center
-    ); // Scaffold
+    final textTheme = Theme.of(context).textTheme;
+    return BackgroundWidget(
+        child: Stack(
+          children: [
+            Align(
+              alignment: const Alignment(-0.2, -0.4),
+              child: Image.asset(Assets.images.banner),
+            ), // Align
+            Align(
+              alignment: const Alignment(0, 0.4),
+              child: Text(
+                'Loading...',
+                style: textTheme.headlineMedium
+                    ?.copyWith(color: const Color(0xFF)),
+            ), // Align
+          ], // Children
+        ) // Stack
+    ); // BackgroundWidget
   }
 }
