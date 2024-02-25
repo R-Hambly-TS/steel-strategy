0b45ed9cd44b95189800ab3dcb36fc1be31f5439
R-Hambly-TS <55469422+R-Hambly-TS@users.noreply.github.com>
2024-02-25 15:43:34 -0400

    Create bg_widget.dart

---
+++ lib/core/widgets/bg_widget.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:steel-strategy/core/constants/customcolors.dart';
+
+class BackgroundWidget extends StatelessWidget {
+  final Widget child;
+  
+  const BackgroundWidget({super.key, required this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: BoxDecoration(
+          gradient: CustomColors.bgGradient),
+          child: child,
+      ), // Container
+    ); // Scaffold
+  }
+}
