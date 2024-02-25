fb922344feab0840046a5cceb4739fecf5bc3a97
R-Hambly-TS <55469422+R-Hambly-TS@users.noreply.github.com>
2024-02-25 15:33:16 -0400

    Update colors.dart

--- lib/core/constants/colors.dart
+++ lib/core/constants/colors.dart
@@ -1,1 +1,9 @@
+import 'package:flutter/cupertino.dart';
+
+class Colors {
+  static LinearGradient bgGradient = LinearGradient(colors: [
+      Color(0xFF64ABFF),
+      Color(0xFF265A97)
+    ]); //LinearGradient
+}
 
