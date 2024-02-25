63c4a2e85498775d66974278a99fda4c2f472688
R-Hambly-TS <55469422+R-Hambly-TS@users.noreply.github.com>
2024-02-25 16:57:02 -0400

    Update and rename place1 to steel_strategy_screen.dart

---
+++ lib/views/game_screen/steel_strategy_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:steel_strategy/core/widgets/bg_widget.dart';
+
+class SteelStrategyScreen extends StatelessWidget {
+  const SteelStrategyScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BackgroundWidget(child: );
+  }
+  
+}
